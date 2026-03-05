a8acbce9dc302adc52e28e6f8c205677f50ea20e
Alyssum-chenyiyi <13162501939@163.com>
2026-03-05 19:21:24 +0800

    climber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -140,6 +140,12 @@ public class RobotContainer {
     controllerlower.rightTrigger()
     .onTrue(robotStatusManager.setStatusCommand(RobotStatus.CrossingBump))
     .onFalse(robotStatusManager.setStatusCommand(RobotStatus.AllTelop));
+
+    controllerlower.a()
+    .onTrue(robotStatusManager.setStatusCommand(RobotStatus.Climbing))
+    .onFalse(robotStatusManager.setStatusCommand(RobotStatus.AllTelop));
+
+
   }
 
   private void configueShooter(){
